adf161f2c81a6f694c989e2c7c159c869e3124c7
shayan <shekshaa.75@gmail.com>
2018-10-02 21:02:30 +0330

    add tsne visualization

--- train.py
+++ train.py
@@ -2,11 +2,12 @@ from load_dataset import *
 from utils import *
 from model import *
 import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
 
 
 flags.DEFINE_string('model', 'gcn_cheby', 'Model string.') # gcn, gcn_cheby
 flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate.')
-flags.DEFINE_integer('epochs', 40, 'Number of epochs to train.')
+flags.DEFINE_integer('epochs', 70, 'Number of epochs to train.')
 flags.DEFINE_integer('hidden1', 36, 'Number of units in hidden layer 1.')
 flags.DEFINE_integer('hidden2', 18, 'Number of units in hidden layer 2.')
 flags.DEFINE_integer('hidden3', 9, 'Number of units in hidden layer 3.')
@@ -60,8 +61,8 @@ for i in range(num_graphs - num_train):
 
 if FLAGS.model == 'gcn_cheby':
     locality1 = 4
-    locality2 = 2
-    locality3 = 1
+    locality2 = 3
+    locality3 = 2
     locality = [locality1, locality2, locality3]  # locality sizes of different blocks
     num_supports = np.max(locality) + 1
     support = chebyshev_polynomials(adj, num_supports - 1)
@@ -131,19 +132,29 @@ with tf.Session() as sess:
 
     plt.plot(test_result)
     plt.show()
-    # print('Storing graph embedding')
-    # embedding_level = 4
-    # with open('./embedding/graph_embedding5.csv', 'w') as csv_file:
-    #     writer = csv.writer(csv_file)
-    #     header = ['id']
-    #     for i in range(FLAGS.hidden3):
-    #         header.append('emb_{}'.format(i))
-    #     writer.writerow(header)
-    #     for i in range(num_graphs):
-    #         feed_dict = construct_feed_dict(sparse_features[i], support, one_hot_labels[i], 1, placeholders)
-    #         feed_dict.update({placeholders['dropout']: 0.})
-    #         embedding = sess.run(model.activations[embedding_level], feed_dict=feed_dict)
-    #         row = [i + 1]
-    #         for item in embedding.tolist()[0]:
-    #             row.append(item)
-    #         writer.writerow(row)
+    print('Storing graph embedding')
+    embedding_level = 4
+    with open('./embedding/graph_embedding9.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        header = ['id']
+        for i in range(FLAGS.hidden3):
+            header.append('emb_{}'.format(i))
+        writer.writerow(header)
+        embeddings = []
+        for i in range(num_graphs):
+            feed_dict = construct_feed_dict(sparse_features[i], support, one_hot_labels[i], 1, placeholders)
+            feed_dict.update({placeholders['dropout']: 0.})
+            embedding = sess.run(model.activations[embedding_level], feed_dict=feed_dict)
+            row = [i + 1]
+            for item in embedding.tolist()[0]:
+                row.append(item)
+            writer.writerow(row)
+            embeddings.append(embedding.tolist()[0])
+
+    print('Plotting t-SNE')
+    embeddings = np.asarray(embeddings)
+    reduced_embedding = TSNE(n_components=2).fit_transform(embeddings)
+    color_names = ['b', 'g', 'r', 'y']
+    colors = [color_names[label] for label in labels]
+    plt.scatter(reduced_embedding[:, 0], reduced_embedding[:, 1], marker='.', c=colors)
+    plt.show()
