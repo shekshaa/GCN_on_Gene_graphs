cab68ad4c015d4f8bf3ac68e7dd7bd4a6b05e8ec
shayan <shekshaa.75@gmail.com>
2019-03-19 21:33:54 +0330

    add GCN with inception modules

--- layers.py
+++ layers.py
@@ -205,3 +205,84 @@ class SumLayer(Layer):
     def _call(self, inputs):
         x = inputs
         return tf.reduce_mean(x, axis=0, keepdims=True)
+
+
+class InceptionGC(Layer):
+    def __init__(self, input_dim, output_dim, locality_sizes, placeholders, is_pool=False, dropout=0.,
+                 sparse_inputs=False, act=tf.nn.relu, bias=False, **kwargs):
+        super(InceptionGC, self).__init__(**kwargs)
+
+        if dropout:
+            self.dropout = placeholders['dropout']
+        else:
+            self.dropout = 0.
+
+        self.act = act
+        self.support = placeholders['support']
+        self.sparse_inputs = sparse_inputs
+        self.bias = bias
+        self.locality_sizes = locality_sizes
+        self.is_pool = is_pool
+        # helper variable for sparse dropout
+        self.num_features_nonzero = placeholders['num_features_nonzero']
+
+        # initialization of filters
+        with tf.variable_scope(self.name + '_vars'):
+            for j in range(len(self.locality_sizes)):
+                for i in range(len(self.support)):
+                    self.vars['gcn_' + str(j) + '_weights_' + str(i)] = glorot([input_dim, output_dim],
+                                                                               name='gcn_' + str(j) + '_weights_' +
+                                                                                    str(i))
+                if self.bias:
+                    self.vars['gcn_' + str(j) + '_bias'] = zeros([output_dim], name='gcn_' + str(j) + '_bias')
+
+        if self.logging:
+            self._log_vars()
+
+    def _call(self, inputs):
+        x = inputs
+
+        # dropout
+        if self.sparse_inputs:
+            x = sparse_dropout(x, 1-self.dropout, self.num_features_nonzero)
+        else:
+            x = tf.nn.dropout(x, 1-self.dropout)
+
+        # convolve
+        self.outputs = []
+        for l in range(len(self.locality_sizes)):
+            supports = []
+            for i in range(self.locality_sizes[l] + 1):
+                pre_sup = dot(x, self.vars['gcn_' + str(l) + '_weights_' + str(i)],
+                              sparse=self.sparse_inputs)
+                support = dot(self.support[i], pre_sup, sparse=True)
+                # collecting results of different degrees
+                supports.append(support)
+
+            # adding results of all degrees as output of one GC layer
+            output = tf.add_n(supports)
+            # bias
+            if self.bias:
+                output += self.vars['bias']
+
+            # collecting outputs of GC layers
+            self.outputs.append(self.act(output))
+
+        # in case of pooling
+        if self.is_pool:
+            # stacking up outputs of all GC layers in a block
+            aux = tf.stack(self.outputs, 1)
+
+            # apply max-out over different GC layers
+            aux2 = tf.nn.pool(aux, [len(self.locality_sizes)], pooling_type='MAX', padding='VALID', data_format='NWC')
+
+            # removing dimensions of size 1
+            aux2 = tf.squeeze(aux2, [1])
+            self.pooled_outputs = aux2
+
+        # not pooling just concatenating outputs of all GC layers
+        else:
+            self.pooled_outputs = tf.concat(self.outputs, 1)
+
+        # final output of GC block
+        return self.pooled_outputs
--- model.py
+++ model.py
@@ -83,7 +83,6 @@ class SimpleGCN(Model):
         super(SimpleGCN, self).__init__(**kwargs)
         self.input_dim = input_dim
         self.inputs = placeholders['features']
-        # self.input_dim = self.inputs.get_shape().as_list()[1]  # To be supported in future Tensorflow versions
         self.placeholders = placeholders
         self.num_class = num_class
         self.optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)
@@ -140,7 +139,6 @@ class CheybyGCN(Model):
         super(CheybyGCN, self).__init__(**kwargs)
         self.input_dim = input_dim
         self.inputs = placeholders['features']
-        # self.input_dim = self.inputs.get_shape().as_list()[1]  # To be supported in future Tensorflow versions
         self.placeholders = placeholders
         self.locality = locality
         self.num_class = num_class
@@ -192,3 +190,91 @@ class CheybyGCN(Model):
     def predict(self):
         return tf.argmax(self.outputs, 1)
 
+
+class InceptionGCN(Model):
+    def __init__(self, placeholders, input_dim, num_class, locality_sizes, is_pool=True, **kwargs):
+        super(InceptionGCN, self).__init__(**kwargs)
+
+        self.inputs = placeholders['features']
+        self.input_dim = input_dim
+        self.num_class = num_class
+        self.placeholders = placeholders
+        self.locality_sizes = locality_sizes
+        self.is_pool = is_pool
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate)
+        self.build()
+
+    def _loss(self):
+        # Weight decay loss
+        for var in self.layers[0].vars.values():
+            self.loss += FLAGS.weight_decay * tf.nn.l2_loss(var)
+
+        self.loss += self.placeholders['weight'] * tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(logits=self.outputs, labels=self.placeholders['labels']))
+
+    def _accuracy(self):
+        correct_prediction = tf.equal(tf.argmax(self.outputs, 1), tf.argmax(self.placeholders['labels']))
+        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, dtype=tf.float32))
+
+    def _build(self):
+        # convolutional layer 1
+        self.layers.append(InceptionGC(input_dim=self.input_dim,
+                                       output_dim=FLAGS.hidden1,
+                                       locality_sizes=self.locality_sizes,
+                                       placeholders=self.placeholders,
+                                       act=tf.nn.relu,
+                                       dropout=True,
+                                       sparse_inputs=True,
+                                       logging=self.logging,
+                                       is_pool=self.is_pool))
+
+        # changing input dim and output dim of layer 1 in different cases of pooling types
+        if not self.is_pool:
+            l2_input_size = len(self.locality_sizes) * FLAGS.hidden1
+            l2_output_size = len(self.locality_sizes) * FLAGS.hidden2
+        else:
+            l2_input_size = FLAGS.hidden1
+            l2_output_size = FLAGS.hidden2
+
+        # convolutional layer 2
+        self.layers.append(InceptionGC(input_dim=l2_input_size,
+                                       output_dim=FLAGS.hidden2,
+                                       locality_sizes=self.locality_sizes,
+                                       placeholders=self.placeholders,
+                                       act=tf.nn.relu,
+                                       dropout=True,
+                                       sparse_inputs=False,
+                                       logging=self.logging,
+                                       is_pool=self.is_pool))
+
+        # changing input dim and output dim of layer 2 in different cases of having skip connections and pooling types
+        if not self.is_pool:
+            l3_input_size = len(self.locality_sizes) * FLAGS.hidden2
+            l3_output_size = len(self.locality_sizes) * FLAGS.hidden3
+        else:
+            l3_input_size = FLAGS.hidden2
+            l3_output_size = FLAGS.hidden3
+
+        # convolutional layer 3
+        self.layers.append(InceptionGC(input_dim=l3_input_size,
+                                       output_dim=FLAGS.hidden3,
+                                       locality_sizes=self.locality_sizes,
+                                       placeholders=self.placeholders,
+                                       act=tf.nn.relu,
+                                       dropout=True,
+                                       sparse_inputs=False,
+                                       logging=self.logging,
+                                       is_pool=self.is_pool))
+
+        self.layers.append(SumLayer())
+        # last dense layer for predicting classes
+        self.layers.append(Dense(input_dim=l3_output_size,
+                                 output_dim=self.num_class,
+                                 placeholders=self.placeholders,
+                                 dropout=False,
+                                 sparse_inputs=False,
+                                 act=lambda x: x,
+                                 bias=True))
+
+    def predict(self):
+        return tf.nn.softmax(self.outputs)
